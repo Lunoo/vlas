9f56b588fce8d87b04467a060ec744c229753945
Zeratulis <churpita.roman@gmail.com>
2016-09-01 15:55:09 +0300

    15.55 - catalog tile

--- js/scripts.js
+++ js/scripts.js
@@ -58,7 +58,11 @@ $(function(){
         return false;
     });
     function left_carusel(carusel){
-       var block_width = $(carusel).find('.carousel-block').outerWidth() + 17;
+        if($('body').width()<768){
+             var block_width = $(carusel).find('.carousel-block').outerWidth() + 9;
+        } else {
+             var block_width = $(carusel).find('.carousel-block').outerWidth() + 17;
+        }
        console.log(block_width);
        $(carusel).find(".carousel-wrapper .carousel-block").eq(-1).clone().prependTo($(carusel).find(".carousel-wrapper")); 
        $(carusel).find(".carousel-wrapper").css({"left":"-"+block_width+"px"});
@@ -67,18 +71,24 @@ $(function(){
        
     }
     function right_carusel(carusel){
-       var block_width = $(carusel).find('.carousel-block').outerWidth() + 17;
+       if($('body').width()<768){
+             var block_width = $(carusel).find('.carousel-block').outerWidth() + 9;
+        } else {
+             var block_width = $(carusel).find('.carousel-block').outerWidth() + 17;
+        }
        $(carusel).find(".carousel-wrapper").animate({left: "-"+ block_width+"px"}, 200, function(){
           $(carusel).find(".carousel-wrapper .carousel-block").eq(0).clone().appendTo($(carusel).find(".carousel-wrapper")); 
           $(carusel).find(".carousel-wrapper .carousel-block").eq(0).remove(); 
           $(carusel).find(".carousel-wrapper").css({"left":"0px"}); 
        }); 
     }
-
+    // MOBILE
+        $('.main-content .slider li, .main-content .carousel figure').each(function(i, item){
+            $(item).css({"background-image":"url("+ $(item).attr('data-src')+")"});
+        })
     // RATING
     var oldRating = $('#review i.active').length; 
     $('#review i').on('mouseover', function(){
-        console.log(oldRating);
         var index = $(this).index();
         $('.rating i').removeClass('active');
         $(this).prevAll().andSelf().addClass('hover');
@@ -93,5 +103,112 @@ $(function(){
         $('.rating i').removeClass('hover');
         $(this).prevAll().andSelf().addClass('active');
         oldRating = $('#review i.active').length;
+    })
+    // NAVIGATION
+    $('.header-nav ul .close').click(function(){
+        if(!$(this).parents('ul').hasClass('active')){
+            $('.header-nav ul').addClass('active');
+        } else {
+            $('.header-nav ul').removeClass('active');
+        }
+    })
+    $('.main-navigation .close').click(function(){
+        if(!$(this).parents('ul').hasClass('active')){
+            $('.main-navigation').addClass('active');
+        } else {
+            $('.main-navigation').removeClass('active');
+        }
+    })
+    $('.catalog h2').click(function(){
+        if($(this).parents('.catalog').hasClass('active')){
+            $(this).parents('.catalog').removeClass('active');
+        } else {
+            $(this).parents('.catalog').addClass('active');
+        }
+    })
+    // RANGE
+    $( '[id*="range"]' ).each(function(i){
+        var $this = $('[id="range-'+i+'"]'),
+            $max = $this.parent().find('.max'),
+            $min = $this.parent().find('.min'),
+            minCost = $this.parent().find('.min').val(),
+            maxCost = $max.val();
+        $this.slider({
+            min: 0,
+            max: maxCost,
+            range: true,
+            values: [ 0, maxCost ],
+            stop: function(event, ui) {
+                jQuery($min).val(jQuery($(this)).slider("values",0));
+                jQuery($max).val(jQuery($(this)).slider("values",1));
+            },
+            slide: function(event, ui){
+                jQuery($min).val(jQuery($(this)).slider("values",0));
+                jQuery($max).val(jQuery($(this)).slider("values",1));
+            }
+        });  
+    });
+    jQuery("input.min").change(function(){
+        var $this = $(this),
+            $parent = $this.parent(),
+            $range = $this.parent().find('[id*="range"]'),
+            value1=$parent.find('.min').val(),
+            value2=$parent.find('.max').val();
+
+        if(parseInt(value1) > parseInt(value2)){
+            value1 = value2;
+            jQuery($range).val(value1);
+        }
+        jQuery($range).slider("values",0,value1);  
+    });
+    jQuery("input.max").change(function(){
+        var $this = $(this),
+            $parent = $this.parent(),
+            $range = $this.parent().find('[id*="range"]'),
+            value1=$parent.find('.min').val(),
+            value2=$parent.find('.max').val(),
+            maxVal=$parent.find('.max').attr('value');
+            
+        if (value2 > maxVal) { value2 = maxVal; jQuery($this).val(maxVal)}
+
+        if(parseInt(value1) > parseInt(value2)){
+            value2 = value1;
+            jQuery($range).val(value2);
+        }
+        jQuery($range).slider("values",1,value2);
+    });
+    $('input.num').keypress(function(event){
+        var key, keyChar;
+        if(!event) var event = window.event;
+        
+        if (event.keyCode) key = event.keyCode;
+        else if(event.which) key = event.which;
+    
+        if(key==null || key==0 || key==8 || key==13 || key==9 || key==46 || key==37 || key==39 ) return true;
+        keyChar=String.fromCharCode(key);
+        
+        if(!/\d/.test(keyChar)) return false;
+    });
+    // FILTER
+    $('.filter-block.dropdown').click(function(){
+        if($(this).hasClass('opened')){
+            $(this).removeClass('opened').addClass('closed');
+        } else {
+            $(this).removeClass('closed').addClass('opened');
+        }
+    })
+    //DROPDOWN
+    $('.dropdown-list span').click(function(){
+        console.log($(this));
+        if($(this).parents('.dropdown-list').hasClass('opened')){
+            $(this).parents('.dropdown-list').removeClass('opened');
+        } else {
+            $(this).parents('.dropdown-list').addClass('opened');
+        }
+    })
+    $('.dropdown-list li').click(function(){
+        var $this = $(this);
+        $this.parents('.dropdown-list').find('span').text($this.text());
+        $this.parents('.dropdown-list').removeClass('opened');
     })
 })
